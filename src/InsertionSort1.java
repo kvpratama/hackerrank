6a0be0805172885c57cd080a0422c5e63da18484
kevindebugging <kevindebugging@gmail.com>
2016-03-16 20:13:25 +0800

    Add InsertionSort1 class

---
+++ src/InsertionSort1.java
@@ -0,0 +1,14 @@
+/**
+ * Created by nv49c on 16/3/16.
+ */
+public class InsertionSort1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void insertionSort1(int[] sortedList){
+        
+    }
+
+}
