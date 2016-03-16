8a2cd3a0f0bac20c2f438e67b3088d59f85fcafe
kevindebugging <kevindebugging@gmail.com>
2016-03-16 20:39:29 +0800

    take input from psvm

--- src/InsertionSort1.java
+++ src/InsertionSort1.java
@@ -1,13 +1,24 @@
-/**
- * Created by nv49c on 16/3/16.
- */
+import java.util.Scanner;
+
+
 public class InsertionSort1 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
 
+        String[] firstLine = sc.nextLine().split(" ");
+        int[] sortedList = new int[firstLine.length];
+
+        for (int i = 0; i < firstLine.length; i++) {
+            sortedList[i] = Integer.parseInt(firstLine[i]);
+        }
+
+        InsertionSort1 i = new InsertionSort1();
+        i.insertionSort1(sortedList);
     }
 
-    public void insertionSort1(int[] sortedList){
+    public void insertionSort1(int[] sortedList) {
 
     }
 
