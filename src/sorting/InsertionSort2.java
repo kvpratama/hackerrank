cceb7b8deecc6152aa5d92c59dd4276c3e048ce0
kevindebugging <kevindebugging@gmail.com>
2016-03-18 19:36:59 +0800

    update psvm and change insertion method based on Hackerrank. add printArray method

--- src/sorting/InsertionSort2.java
+++ src/sorting/InsertionSort2.java
@@ -1,13 +1,29 @@
 package sorting;
 
+import java.util.Scanner;
+
 public class InsertionSort2 {
 
     public static void main(String[] args) {
-        insertionSort2();
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for (int i = 0; i < s; i++) {
+            ar[i] = in.nextInt();
+        }
+        insertionSortPart2(ar);
     }
 
-    public static void insertionSort2(){
+    public static void insertionSortPart2(int[] ar) {
 
+
+    }
+
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
     }
 
 }
