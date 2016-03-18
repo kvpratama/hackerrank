58c0413b9f3d72eed4a938a8e739b83ed0706fd3
kevindebugging <kevindebugging@gmail.com>
2016-03-18 19:27:19 +0800

    add insertion sort 2 class and add psvm and method declaration

---
+++ src/sorting/InsertionSort2.java
@@ -0,0 +1,13 @@
+package sorting;
+
+public class InsertionSort2 {
+
+    public static void main(String[] args) {
+        insertionSort2();
+    }
+
+    public static void insertionSort2(){
+
+    }
+
+}
