688f4bdbb2149811b32154607f29abd8b729873f
kevindebugging <kevindebugging@gmail.com>
2016-04-15 20:01:25 +0800

    add lomuto algorithm

--- src/algorithms/sorting/QuicksortInPlace.java
+++ src/algorithms/sorting/QuicksortInPlace.java
@@ -13,7 +13,7 @@ public class QuicksortInPlace {
             return ar;
         } else if (size == 2) {
             if (ar.get(firstIndex) > ar.get(secondIndex - 1)) {
-                ar = swap(ar, firstIndex, secondIndex-1);
+                ar = swap(ar, firstIndex, secondIndex - 1);
             }
             printArray(ar);
             return ar;
@@ -48,6 +48,33 @@ public class QuicksortInPlace {
         System.out.println("");
     }
 
+    static int partition(ArrayList<Integer> ar, int lo, int hi) {
+        int pivot = ar.get(hi - 1);
+        int i = lo;
+        boolean print = false;
+        for (int j = lo; j < hi; j++) {
+            if (ar.get(j) < pivot) {
+                swap(ar, i, j);
+                i++;
+//                print = true;
+            }
+        }
+        swap(ar, i, hi - 1);
+//        System.out.println(i);
+//        if (print) {
+            printArray(ar);
+//        }
+        return i;
+    }
+
+    static void quickSortLomuto(ArrayList<Integer> ar, int lo, int hi) {
+        if (lo < hi) {
+            int p = partition(ar, lo, hi);
+            quickSortLomuto(ar, lo, p);
+            quickSortLomuto(ar, p + 1, hi);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -55,7 +82,9 @@ public class QuicksortInPlace {
         for (int i = 0; i < n; i++) {
             ar.add(i, in.nextInt());
         }
-        quickSortInPlace(ar, 0, ar.size());
+//        quickSortInPlace(ar, 0, ar.size());
+//        partition(ar, 0, ar.size());
+        quickSortLomuto(ar, 0, ar.size());
     }
 
 }
