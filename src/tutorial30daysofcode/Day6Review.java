2f827e3505b8c1baf09ef11110cdc065cfd02a86
kvpratama <kvpratama@gmail.com>
2016-05-02 20:39:05 +0800

    Day 4 : Class vs Instance

--- src/tutorial30daysofcode/Day6Review.java
+++ src/tutorial30daysofcode/Day6Review.java
@@ -7,38 +7,22 @@ public class Day6Review {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        in.nextLine();
-        String input[] = new String[N];
-        for (int i = 0; i < N; i++) {
-            input[i] = in.nextLine();
-        }
-        for (int i = 0; i < N; i++) {
-            char reformat[] = input[i].toCharArray();
-            char evenOdd[] = new char[reformat.length];
-            int charLength = evenOdd.length;
-            int even = 0; int odd = charLength/2;
-            if (charLength%2 != 0) {
-                odd++;
-            }
+        StringBuilder even, odd;
 
-            int middle = odd;
-            for (int j = 0; j < charLength; j++) {
-                if (j%2 == 0){
-                    evenOdd[even] = reformat[j];
-                    even++;
-                }else {
-                    evenOdd[odd] = reformat[j];
-                    odd++;
+        for (int i = 0; i < N; i++) {
+            even = new StringBuilder();
+            odd = new StringBuilder();
+            char c[] = in.next().toCharArray();
+            int len = c.length;
+            for (int j = 0; j < len; j++) {
+                if (j % 2 == 0) {
+                    even.append(c[j]);
+                } else {
+                    odd.append(c[j]);
                 }
             }
-
-            for (int j = 0; j < charLength; j++) {
-                if (j == middle){
-                    System.out.print(" ");
-                }
-                System.out.print(evenOdd[j]);
-            }
-            System.out.println();
+            System.out.println(even + " " + odd);
         }
+        in.close();
     }
 }
