b7c9e76a03f31baada3e18d2192f57cb17408bf5
kvpratama <kvpratama@gmail.com>
2016-05-31 20:29:51 +0800

    Day 29: Bitwise AND

--- src/tutorial30daysofcode/Day29BitwiseAND.java
+++ src/tutorial30daysofcode/Day29BitwiseAND.java
@@ -15,13 +15,14 @@ public class Day29BitwiseAND {
 
     static int maximumAndSmallerK(int N, int K) {
         int max = 0;
-        mainloop:
+        mainloop: //loop declaration to break from nested loop
+        //since Set S start with 1, declare i = 1;
         for (int i = 1; i <= N; i++) {
             for (int j = i + 1; j <= N; j++) {
                 int tmp = i & j;
                 if (tmp > max && tmp < K) {
                     max = tmp;
-                    if (max == K - 1) {
+                    if (max == K - 1) {//maximum possible value
                         break mainloop;
                     }
                 }
@@ -32,38 +33,28 @@ public class Day29BitwiseAND {
 }
 /*
 Objective
-        Welcome to the last day! Today, we're discussing bitwise operations. Check out the Tutorial tab for learning materials maximumAndSmallerK an instructional video!
 
-        Task
-        Given set . Find two integers,  maximumAndSmallerK  (where ), from set  such that the value of  is the maximum possible maximumAndSmallerK also less than a given integer, . In this case,  represents the bitwise AND operator.
+    Task
+    Given set S = {1,2,3,...N} . Find two integers,  A and B  (where A < B), from set  such that the value of A&B is the maximum possible and also less than a given integer, K. In this case, & represents the bitwise AND operator.
 
-        Input Format
+    Input Format
 
-        The first line contains an integer, , the number of test cases.
-        Each of the  subsequent lines defines a test case as  space-separated integers,  maximumAndSmallerK , respectively.
+    The first line contains an integer, T , the number of test cases.
+    Each of the T subsequent lines defines a test case as 2 space-separated integers,  N and K , respectively.
 
-        Constraints
+    Output Format
 
-        Output Format
+    For each test case, print the maximum possible value of  on a new line.
 
-        For each test case, print the maximum possible value of  on a new line.
+    Sample Input
 
-        Sample Input
+    3
+    5 2
+    8 5
+    2 2
+    Sample Output
 
-        3
-        5 2
-        8 5
-        2 2
-        Sample Output
-
-        1
-        4
-        0
-        Explanation
-
-
-
-        All possible values of  maximumAndSmallerK  are:
-
-        The maximum possible value of  that is also  is , so we print  on a new line.
+    1
+    4
+    0
 */
